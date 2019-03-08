258c8195201263a8cf3a60f950d42b470a35477c
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-03-08 21:09:59 +0000

    Extracted code into a method that takes a file_path into parameter. Attempted to make a script that iterated through the files but was projected to take too much time

--- C1Analysis.py
+++ C1Analysis.py
@@ -1,57 +1,62 @@
 
-lines = []
-lines_after_split = []
-notes = []
-#Used to determine how many taps and holds there are
-note_checker = []
-link_counter = []
-taps = 0
-holds = 0
-drags = 0
+file_path = "/home/sammy/Documents/C1/easy-chart-1 (c1).txt"
 
-with open("easy-chart-1 (c1).txt", 'rt') as file:
-    for line in file:
-        # print(line)
-        lines.append(line.rstrip('\n'))
+def ExtractNotes(file_path):
 
-    #Gets rid of \t (TAB) in the file and splits the line into an array
-    for x in lines:
-        new_lines = x.replace('\t', ' ')
-        lines_after_split.append(new_lines.split(' '))
-        if(x.find('LINK') != -1):
-            drags += 1
-        # print(x)
+    lines = []
+    lines_after_split = []
+    notes = []
+    #Used to determine how many taps and holds there are
+    note_checker = []
+    link_counter = []
+    taps = 0
+    holds = 0
+    drags = 0
 
+    with open(file_path, 'rt') as file:
+        for line in file:
+            # print(line)
+            lines.append(line.rstrip('\n'))
 
-    #Filter between notes
-    for y in lines_after_split:
-        for x in y:
-            if (x.find('NOTE') != -1):
-                notes.append(y)
+        # Gets rid of \t (TAB) in the file and splits the line into an array
+        for x in lines:
+            new_lines = x.replace('\t', ' ')
+            lines_after_split.append(new_lines.split(' '))
+            if (x.find('LINK') != -1):
+                drags += 1
+            # print(x)
+
+        # Filter between notes
+        for y in lines_after_split:
+            for x in y:
+                if (x.find('NOTE') != -1):
+                    notes.append(y)
+                else:
+                    break
+
+        # Get the value which will determine whether it's a tap or a hold
+        for note in notes:
+            value = float(note[4])
+            note_checker.append(value)
+
+        for num in note_checker:
+            if (num > 0.0):
+                holds += 1
+            elif (num == 0.0):
+                taps += 1
             else:
                 break
-
-    #Get the value which will determine whether it's a tap or a hold
-    for note in notes:
-        value = float(note[4])
-        note_checker.append(value)
-
-    for num in note_checker:
-        if(num > 0.0):
-            holds +=1
-        elif(num == 0.0):
-            taps +=1
-        else:
-            break
+    print("BPM:" + str(float(lines_after_split[1][1])))
+    print("Taps:" + str(taps))
+    print("Drags:" + str(drags))
+    print("Holds:" + str(holds))
 
 
-print("Taps:" + str(taps))
-print("BPM:" + str(float(lines_after_split[1][1])))
-print("Drags:" + str(drags))
-print("Holds:" + str(holds))
+ExtractNotes(file_path)
 # print(lines_after_split)
 # print(notes)
 # print(note_checker)
+
 
 
 
